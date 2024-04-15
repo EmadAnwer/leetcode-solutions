b8b3323b2c14eeabbd88f5b2f1f474615c14f9e6
Emad <emadanwer.official@gmail.com>
2024-04-15 11:59:41 +0200

    [217] [Time Beats: 15.96%] [Memory Beats: 25.35%] - LeetPush

---
+++ 217-Contains-Duplicate.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        \\\
+        :type nums: List[int]
+        :rtype: bool
+        \\\
+        s = set(nums)
+
+        return not len(s) == len(nums)
+        
