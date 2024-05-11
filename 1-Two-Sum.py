6c8d13ba914a05be2ffe4fb2b57eebe2d71e2f48
Emad <emadanwer.official@gmail.com>
2024-05-11 20:26:58 +0300

    [1] [Time Beats: 95.17%] [Memory Beats: 21.01%] - LeetPush

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -5,7 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         \\\
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i != j and  nums[i] + nums[j] == target:
-                    return [i, j]
+        num_dict = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
