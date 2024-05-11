9d4e4185927b7bea0efe8986bec94e26d9679c74
Emad <emadanwer.official@gmail.com>
2024-05-11 21:39:22 +0300

    [15] [Time Beats: 72.19%] [Memory Beats: 5.29%] - LeetPush

---
+++ 15-3Sum.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def threeSum(self, nums):
+        \\\
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        \\\
+        nums.sort()
+        t_set = set()
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue  # Skip duplicates
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+                if total > 0:
+                    r -= 1
+                elif total < 0:
+                    l += 1
+                else:
+                    t_set.add((nums[i], nums[l], nums[r]))
+                    l += 1
+                    r -= 1
+        return [list(t) for t in t_set]
