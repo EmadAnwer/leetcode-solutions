8424cb73c2b68e2d49b59d2d99e3c51a7ddaae27
Emad <emadanwer.official@gmail.com>
2024-05-16 14:33:20 +0300

    [20] [Time Beats: 93.81%] [Memory Beats: 25.86%] - LeetPush

---
+++ 20-Valid-Parentheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        \\\
+        :type s: str
+        :rtype: bool
+        \\\
+        if len(s) % 2 != 0:
+            return False
+
+        stack = []
+        parentheses = {
+            \(\:\)\,
+             \[\:\]\,
+             \{\:\}\
+        }
+
+        for p in s:
+            if p in parentheses:
+                stack.append(p)
+            elif len(stack) == 0 or parentheses[stack.pop()] != p:
+                return False
+        return len(stack) == 0
