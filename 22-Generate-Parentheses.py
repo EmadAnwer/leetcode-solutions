19a20f1c76b7a9dc6f353c6fcf34d337434b057b
Emad <emadanwer.official@gmail.com>
2024-05-19 12:39:06 +0300

    [22] [Time Beats: 41.88%] [Memory Beats: 70.57%] - LeetPush

---
+++ 22-Generate-Parentheses.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        \\\
+        :type n: int
+        :rtype: List[str]
+        \\\
+        stack = []
+        res = []
+
+
+        def bt(o, c):
+            if o == c == n:
+                res.append(\\.join(stack))
+                return
+            
+
+            if o < n:
+                stack.append(\(\)
+                bt(o+1, c)
+                stack.pop()
+            
+            if c < o:
+                stack.append(\)\)
+                bt(o, c + 1)
+                stack.pop()
+        
+        bt(0,0)
+
+        return res
