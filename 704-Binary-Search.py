2c0f7cde6e33ae5b7001b9fc7b54223b7fcd81eb
Emad <emadanwer.official@gmail.com>
2024-05-23 12:16:03 +0300

    [704] [Time Beats: 24.72%] [Memory Beats: 99.44%] - LeetPush

--- 704-Binary-Search.py
+++ 704-Binary-Search.py
@@ -1,22 +1,16 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-
-        l = 0
-        r = len(nums) - 1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         
+        l, r = 0, len(nums) - 1
+
+        m = (l + r) // 2
         while l <= r:
-            mid = (l + r) // 2
-            if target == nums[mid]:
-                return mid
-            elif target > nums[mid]:
-                l = mid + 1
+            if nums[m] == target:
+                return m
+            elif nums[m] > target:
+                r = m - 1
             else:
-                r = mid - 1 
-        
-        return -1
+                l = m + 1
+            m = (l + r) // 2
 
+        return -1
