61770056ecaea27d0c1634b771882e9d7d912b2e
Emad <emadanwer.official@gmail.com>
2024-07-16 23:27:30 +0300

    [242] [Time Beats: 76.85%] [Memory Beats: 76.72%] - LeetPush

--- 242-Valid-Anagram.py
+++ 242-Valid-Anagram.py
@@ -1,3 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        
+
+        if len(s) != len(t):
+            return False
+
+        
+        chars = [0] * 26
+
+        for c1, c2 in zip(s, t):
+            chars[97-ord(c1)] += 1
+            chars[97-ord(c2)] -= 1
+
+        for n in chars:
+            if n != 0:
+                return False
+        return True
