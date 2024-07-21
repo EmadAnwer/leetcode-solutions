95383a52a99f5dfc2ef6626145fd67c5c95ff853
Emad <emadanwer.official@gmail.com>
2024-07-21 10:27:22 +0300

    [125] [Time Beats: 56.27%] [Memory Beats: 54.29%] - LeetPush

--- 125-Valid-Palindrome.py
+++ 125-Valid-Palindrome.py
@@ -1,11 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-        s = [c.lower() for c in s if 'a' <= c <= 'z' or 'A' <= c <= 'Z' or '0' <= c <= '9']
         l, r = 0, len(s) - 1
 
-        while l <= r:
-            if s[l] != s[r]:
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
                 return False
             l += 1
             r -= 1
