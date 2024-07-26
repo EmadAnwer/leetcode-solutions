fe7746275f06bc78be1effdbc1906c3e3f4a8280
Emad <emadanwer.official@gmail.com>
2024-07-26 15:44:36 +0300

    [1334] [Time Beats: 60.87%] [Memory Beats: 9.39%] - LeetPush

---
+++ 1334-Find-the-City-With-the-Smallest-Number-of-Neighbors-at-a-Threshold-Distance.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+import heapq
+from typing import List
+
+
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        adj = defaultdict(list)
+
+        for v1, v2, w in edges:
+            adj[v1].append((v2, w))
+            adj[v2].append((v1, w))
+        
+        def dijkstra(start):
+            heap = [(0, start)]
+            visited = set()
+
+            while heap:
+                dist, node = heapq.heappop(heap)
+
+                if node in visited:
+                    continue
+
+                visited.add(node)
+
+                for neighbor, weight in adj[node]:
+                    new_dist = dist + weight
+                    if new_dist <= distanceThreshold:
+                        heapq.heappush(heap, (new_dist, neighbor))
+            return len(visited) - 1
+        
+
+        min_cities = n
+        min_city = -1
+
+        for i in range(n):
+            cities = dijkstra(i)
+            if cities <= min_cities:
+                min_cities = cities
+                min_city = i
+        
+        return min_city
